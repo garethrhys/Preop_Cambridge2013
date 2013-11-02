1ec3b3fcfe7f21d4fa43f2f1bb470ae663ec88c1
Andrew Davey <andrew@equin.co.uk>
2013-11-02 17:15:23 +0000

    Refactor

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
 var app = angular.module("app", []);
 
-app.value("questionData", 
-{
-  title: "Example questions",
-  questions: [
-    {
-      id: 1,
-      text: "Have you had an operation before?",
-      type: "boolean",
-      hint: "Anything at all..."
-    },
-    {
-      condition: "q[1] === 'yes'",
-      id: 2,
-      text: "When was your last operation?",
-      type: "text"
-    },
-    {
-      id: 3,
-      text: "How often do you hack?",
-      type: "single-choice",
-      choices: [
-        { id: "0", text: "Never" },
-        { id: "1", text: "Once a week" },
-        { id: "2", text: "Once a month", flag: "RED" },
-      ]
-    },
-    {
-      id: 4,
-      text: "What medication are you taking?",
-      type: "multi-choice",
-      choices: [
-        { id: "x", text: "XXX" },
-        { id: "y", text: "YYY" },
-        { id: "z", text: "ZZZ" }
-      ]
-    }
-  ]
-});
+app.factory("questionData", ["$http", function($http) {
+  return $http.get("/questions.json").then(function(r){ return r.data; });
+}]);
 
 app.factory("publicKey", function() {
   return $("#public-key").text();
@@ -48,8 +13,14 @@ app.controller(
   ["$scope", "$anchorScroll", "$location", "$timeout", "questionData", "$parse", "encrypt",
   function($scope, $anchorScroll, $location, $timeout, questionData, $parse, encrypt) {
 
-  var remainingQuestions = questionData.questions.slice(0); // copy the questions array so we destructive update it
+  var remainingQuestions = [];
   var activeQuestions = [];
+
+  $scope.gotQuestions = false;
+  questionData.then(function(data) {
+    remainingQuestions.push.apply(remainingQuestions, data.questions);
+    $scope.gotQuestions = true;
+  });
 
   $scope.title = questionData.title;
   $scope.patient = {};
@@ -113,59 +84,74 @@ app.controller(
     $anchorScroll();
   }
 
-  function finished() {
-    var questions = activeQuestions.map(function(q) {
-      var answer;
-      if (q.type === "multi-choice") {
-        answer = q.choices
-          .filter(function(choice) { return choice.selected; })
-          .map(function(choice) { return choice.id; });
-      } else {
-        answer = q.answer
-      }
-      var output = {
-        id: q.id,
-        question: q.text,
-        answer: answer
-      };
-      if (q.comments) {
-        output.comments = q.comments;
-      }
-      return output;
-    });
+  function questionOutput(q) {
+    var answer;
+    if (q.type === "checklist") {
+      answer = q.options
+        .filter(function(option) { return option.selected; })
+        .map(function(option) { return option.text; });
+    } else {
+      answer = q.answer
+    }
 
-    var flags = activeQuestions.map(function(q) {
-      switch (q.type) {
-        case "single-choice":
-          var flaggedChoice = q.choices.filter(function(choice) { return choice.id === q.answer && choice.flag; })[0];
-          if (flaggedChoice) {
-            return [{
-              flag: flaggedChoice.flag,
-              question: q.text,
-              answer: flaggedChoice.id
-            }];
-          } else {
-            return [];
-          }
+    var output = {
+      id: q.id,
+      question: q.text,
+      answer: answer
+    };
 
-        case "multi-choice":
-          return q.choices
-            .filter(function(choice) { return choice.selection && choice.flag; })
-            .map(function(choice) {
-              return {
-                flag: flaggedChoice.flag,
-                question: q.text,
-                answer: flaggedChoice.id
-              };
-            });
+    if (q.comments) {
+      output.comments = q.comments;
+    }
 
-        default:
+    return output;
+  }
+
+  function getQuestionFlags(q) {
+    switch (q.type) {
+      case "radiolist":
+        var flagged = q.options
+          .filter(function(option) {
+            return option.id === q.answer && option.flag;
+          })
+          [0];
+
+        if (flagged) {
+          return [{
+            flag: flagged.flag,
+            question: q.text,
+            answer: flagged.text
+          }];
+        } else {
           return [];
-      }
-    });
+        }
 
+      case "checklist":
+        return q.options
+          .filter(function(option) { return option.selected && option.flag; })
+          .map(function(option) {
+            return {
+              flag: option.flag,
+              question: q.text,
+              answer: option.text
+            };
+          });
+
+      default:
+        return [];
+    }
+  }
+
+  function getAllFlags() {
+    var flags = activeQuestions.map(getQuestionFlags);
     // Flatten all flag arrays in one
     flags = [].concat.apply([], flags);
+    return flags;
+  }
+
+  function finished() {
+    var questions = activeQuestions.map(questionOutput);
+    var flags = getAllFlags();
 
     $scope.output = {
       patient: $scope.patient,
@@ -173,11 +159,8 @@ app.controller(
       questions: questions
     };
 
-
     $scope.encrypted = encrypt(JSON.stringify($scope.output));
-
     // TODO: post to relay server
-
     $scope.finished = true;
     $location.hash("");
   }
